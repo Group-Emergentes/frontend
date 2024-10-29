156fc173795142d7cc43ffe50a4a4c2b643df5c2
Angie_Naye_Ruiz <89089445+Angie-RC@users.noreply.github.com>
2024-10-29 14:31:54 -0500

    add profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from "./features/auth/pages/login/login.component";
 import {RegisterComponent} from "./features/auth/pages/register/register.component";
 import {DashboardComponent} from "./features/dashboard/pages/dashboard/dashboard.component";
+import {ProfileComponent} from "./features/profile/profile/profile.component";
 
 
 export const routes: Routes = [
@@ -10,5 +11,5 @@ export const routes: Routes = [
   {path: 'login', component:LoginComponent},
   {path: 'register', component:RegisterComponent},
   {path: 'dashboard', component:DashboardComponent},
-
+  {path: 'profile', component:ProfileComponent},
 ];
